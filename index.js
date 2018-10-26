a3dd0c8ee3a13b14bebeb8d2d99aacf39d745974
Rufat <rufatmdl@gmail.com>
2018-10-26 22:11:21 +0400

    config bug fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import PubSub from 'pubsub-js';
 
 const noux = function (props) {
-    this.logs = props.log || true;
+    this.logs = typeof props === 'object' && typeof props.log === 'boolean' ? props.log : true;
     this.cache = {};
     this.errorList = {
         prefix: 'Noux.js [ERROR]',
@@ -30,38 +30,56 @@ function componentChecker(self, n, d) {
             return {success: false, msg: `${self.errorList.prefix} ${self.errorList.invalid_component}\n    obj: ${d}`};
         }
     } catch (ex) {
-        return {success: false, msg: `${self.errorList.prefix} ${self.errorList.internal_error}\n   obj: ${d}`};
+        return {
+            success: false,
+            msg: `${self.errorList.prefix} ${self.errorList.internal_error}\n   obj: ${n} | ${d}   ex: ${ex.toString()}`
+        };
     }
 }
 
 noux.prototype.setState = function (props) {
-    const isValid = componentChecker(this, props.target, props);
-    if (!isValid.success) {
-        if (this.logs) console.error(isValid.msg);
+    try {
+        const isValid = componentChecker(this, props.target, props);
+        if (!isValid.success) {
+            if (this.logs) console.error(isValid.msg);
+            return null;
+        }
+        PubSub.publish("_u", {target: props.target, variable: props.state, value: props.value});
+    } catch (ex) {
+        if (this.logs) console.error(`${self.errorList.prefix} ${self.errorList.internal_error}\n   obj: ${props}   ex: ${ex.toString()}`);
         return null;
     }
-    PubSub.publish("_u", {target: props.target, variable: props.state, value: props.value});
 };
 
 noux.prototype.state = function (n, p) {
-    const isValid = componentChecker(this, n, {n, p});
-    if (!isValid.success) {
-        if (this.logs) console.error(isValid.msg);
-        return null;
-    }
-    if (typeof this.cache[n].state[p] !== 'undefined') {
-        return this.cache[n].state[p];
-    } else {
-        if (this.logs) console.error(`${this.errorList.prefix} ${this.errorList.invalid_state}`);
+    try {
+        const isValid = componentChecker(this, n, {n, p});
+        if (!isValid.success) {
+            if (this.logs) console.error(isValid.msg);
+            return null;
+        }
+        if (typeof this.cache[n].state[p] !== 'undefined') {
+            return this.cache[n].state[p];
+        } else {
+            if (this.logs) console.error(`${this.errorList.prefix} ${this.errorList.invalid_state}`);
+            return null;
+        }
+    } catch (ex) {
+        if (this.logs) console.error(`${self.errorList.prefix} ${self.errorList.internal_error}\n   obj: ${n} | ${p}   ex: ${ex.toString()}`);
         return null;
     }
 };
 
 noux.prototype.init = function (props) {
-    this.cache[props.name] = props.self;
-    PubSub.subscribe('_u', (v, d) => {
-        this.cache[d.target].setState({[d.variable]: d.value});
-    });
+    try {
+        this.cache[props.name] = props.self;
+        PubSub.subscribe('_u', (v, d) => {
+            this.cache[d.target].setState({[d.variable]: d.value});
+        });
+    } catch (ex) {
+        if (this.logs) console.error(`${self.errorList.prefix} ${self.errorList.internal_error}\n   ex: ${ex.toString()}`);
+        return null;
+    }
 };
 
 export default noux;
